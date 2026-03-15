f62a0f2ea84e680f61a384d15272539d564c4f12
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:30 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Dapr runtime - APIs for building secure and reliable microservices";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          go
+          gopls
+          gotools
+          # add docker, kubectl etc. for services
+        ];
+      };
+    });
+}
